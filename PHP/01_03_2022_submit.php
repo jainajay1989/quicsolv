b9977355bddb67f3a4fdf6a606b003eba092db8c
Ajay Jain <jainajay1989@gmail.com>
2022-03-01 16:31:53 +0530

    Upload

--- PHP/01_03_2022_submit.php
+++ PHP/01_03_2022_submit.php
@@ -14,15 +14,46 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 /* This code is used to connect with database */
+if(!empty($_POST)) {
+    $sql = "INSERT INTO Persons (firstname, lastname, address, city)
+    VALUES ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['address']."', '".$_POST['city']."')";
 
-$sql = "INSERT INTO Persons (firstname, lastname, address, city)
-VALUES ('".$_POST['firstname']."', '".$_POST['lastname']."', '".$_POST['address']."', '".$_POST['city']."')";
-
-if (mysqli_query($conn, $sql)) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 }
 
-mysqli_close($conn);
+/* Code used to fetch the data */
+$sql = "Select * from Persons";
+$data = mysqli_query($conn, $sql);
+
+while($row = mysqli_fetch_assoc($data)) {
+    $records[] = $row;
+}
+/* Code used to fetch the data */
 ?>
+<table class="table" border="1">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Address</th>
+            <th>City</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($records as $val) { ?>
+        <tr>
+            <td><?=$val['PersonID']?></td>
+            <td><?=$val['FirstName']?></td>
+            <td><?=$val['LastName']?></td>
+            <td><?=$val['Address']?></td>
+            <td><?=$val['City']?></td>
+            <td><a href="edit.php?personId=<?=$val['PersonID'];?>">Edit</a> | <a href="delete.php?personId=<?=$val['PersonID'];?>">Delete</a></td>
+        </tr>
+        <?php } ?>
+    </tbody>
+</table>
