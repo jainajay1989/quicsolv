a6c1e4de87c0a7b4f528b77bfd6947f4470466cc
Ajay Jain <jainajay1989@gmail.com>
2022-03-01 17:06:28 +0530

    Upload

---
+++ Tasks/Task-01-03-2022.php
@@ -0,0 +1,17 @@
+Task - 01/03/2022
+
+Task 1:
+
+1) Add Input field Gender(Male & Female) & Hobbies(Cricket, Singing, Dancing) in the existing form
+2) Save it into the database by creating 2 extra columns gender & hobbies in database.
+3) List Gender & Hobbies in same table with fields.
+
+Task 2: 
+
+1) When we click on edit link it opens new edit form with prefilled values from database for specific person ID. 
+2) On submit it would update the data in the table for specific fields
+3) After update we will redirect(01_03_2022_submit.php) on list page.
+
+Taks 3: 
+
+1) When we click on delete link then it should delete the record and redirect(01_03_2022_submit.php) on list page.
