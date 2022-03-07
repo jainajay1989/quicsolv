e0b571b0b14f021a19a47ec0635b33ca28dc1341
Ajay Jain <jainajay1989@gmail.com>
2022-03-07 18:07:17 +0530

    UP

---
+++ Tasks/Task-07-3-2022.php
@@ -0,0 +1,5 @@
+Task:
+
+1) Write migration to create table employee with fields id, name, email.
+2) Create form to post data using view and controller
+3) Write the code to save data into table using laravel.
